a40264a14e3953a3ccbf5efb8a65ad7e47b197aa
vektorel249 <vektorel249@outlook.com>
2025-01-26 13:30:09 +0300

    SELECT örnekleri

--- Ders Örnekleri.sql
+++ Ders Örnekleri.sql
@@ -114,3 +114,171 @@ INNER JOIN Categories AS c ON p.CategoryID = c.CategoryID
 WHERE c.CategoryName = 'Beverages'
 
 -- FOREIGN KEY : Baþka bir tablodan alýnan PRIMARY KEY'in mevcut tabloda kullanýldýðý hal
+SELECT 
+	p.ProductName, 
+	od.UnitPrice, 
+	od.Quantity, 
+	od.UnitPrice * od.Quantity AS Summary, 
+	o.OrderDate 
+FROM [Order Details] AS od
+INNER JOIN Products AS p ON od.ProductID = p.ProductID
+INNER JOIN Orders AS o ON od.OrderID = o.OrderID
+WHERE o.OrderID = 10260
+
+--- Verilen sipariþ numarasýna göre hangi personelin hangi müþteriye satýþ yaptýðýný gösteren sorgu
+SELECT 
+	CONCAT(e.FirstName, ' ', e.LastName) Employee,
+	c.ContactName Customer
+FROM Orders o
+INNER JOIN Employees e ON o.EmployeeID = e.EmployeeID
+INNER JOIN Customers c ON o.CustomerID = c.CustomerID
+WHERE o.OrderID = 10251
+
+--- 1997 Ocak ayýnda Almanya'ya yapýlan sipariþlerin numaralarý ve 
+--- sipariþ tarihleri ve sevkiyat tarihleri
+SELECT o.OrderID, o.OrderDate, o.ShippedDate
+FROM Orders o
+INNER JOIN Customers c ON o.CustomerID = c.CustomerID
+WHERE 
+	o.OrderDate BETWEEN '1997-01-01 00:00:00' AND '1997-01-31 23:59:59' AND
+	c.Country = 'Germany'
+
+-- Ernst Handel firmasýnýn 1997 yýlýnda verdiði sipariþler
+SELECT o.OrderID, o.OrderDate FROM Orders o
+INNER JOIN Customers c ON o.CustomerID = c.CustomerID
+WHERE 
+	YEAR(o.OrderDate) = 1997 AND c.CompanyName = 'Ernst Handel'
+
+-- Ýçecek kategorisinde Nancy'nin sattýðý ürünler 
+-- ve tarihe göre elde edilen gelir
+-- Cool Yöntem
+SELECT 
+	  o.OrderDate, 
+	  od.Quantity * od.UnitPrice Summary
+FROM [Order Details] od
+INNER JOIN Orders o ON od.OrderID = o.OrderID
+INNER JOIN Products p ON od.ProductID = p.ProductID
+WHERE p.CategoryID = 1 AND 
+	  o.EmployeeID = 1
+
+-- Keko Yöntem
+SELECT 
+	  o.OrderDate, 
+	  od.Quantity * od.UnitPrice Summary
+FROM [Order Details] od
+INNER JOIN Orders o ON od.OrderID = o.OrderID
+INNER JOIN Products p ON od.ProductID = p.ProductID
+INNER JOIN Categories c ON p.CategoryID = c.CategoryID
+INNER JOIN Employees e ON o.EmployeeID = e.EmployeeID
+WHERE c.CategoryName = 'Beverages' AND 
+	  e.FirstName = 'Nancy' AND
+	  e.LastName = 'Davolio'
+
+-- INNER SELECT QUERY : SUBQUERY
+SELECT 
+	  o.OrderDate, 
+	  od.Quantity * od.UnitPrice Summary
+FROM [Order Details] od
+INNER JOIN Orders o ON od.OrderID = o.OrderID
+INNER JOIN Products p ON od.ProductID = p.ProductID
+WHERE p.CategoryID = (
+			SELECT CategoryID FROM Categories 
+			WHERE CategoryName = 'Beverages'
+	  ) AND 
+	  o.EmployeeID = (
+			SELECT EmployeeID FROM Employees
+			WHERE FirstName = 'Nancy' AND LastName = 'Davolio'
+	  )
+-- PRO Yöntem
+DECLARE @CategoryId INT;
+DECLARE @EmployeeId INT;
+SET @CategoryId = (
+			SELECT CategoryID FROM Categories 
+			WHERE CategoryName = 'Beverages'
+	  );
+SET @EmployeeId = (
+			SELECT EmployeeID FROM Employees
+			WHERE FirstName = 'Nancy' AND LastName = 'Davolio'
+	  )
+SELECT 
+	  o.OrderDate, 
+	  od.Quantity * od.UnitPrice Summary
+FROM [Order Details] od
+INNER JOIN Orders o ON od.OrderID = o.OrderID
+INNER JOIN Products p ON od.ProductID = p.ProductID
+WHERE p.CategoryID = @CategoryId AND 
+	  o.EmployeeID = @EmployeeId
+
+-- Müþteriye göre bir sipariþin detayý
+-- DOðrudan ID ile eriþim
+SELECT p.ProductName, od.UnitPrice * od.Quantity Summary
+FROM [Order Details] od
+INNER JOIN Products p ON od.ProductID = p.ProductID
+INNER JOIN Orders o ON od.OrderID = o.OrderID
+WHERE o.CustomerID = 'ALFKI'
+
+-- Mecburen SUBQUERY ile eriþim
+SELECT p.ProductName, od.UnitPrice * od.Quantity Summary
+FROM [Order Details] od
+INNER JOIN Products p ON od.ProductID = p.ProductID
+INNER JOIN Orders o ON od.OrderID = o.OrderID
+WHERE o.CustomerID = (SELECT CustomerID FROM Customers WHERE CompanyName = 'Alfreds Futterkiste')
+
+-- BÝR JOIN DAHA EKLERSEK - CHATGPT BUNA DESTEK VERDÝ :D
+SELECT p.ProductName, od.UnitPrice * od.Quantity Summary
+FROM [Order Details] od
+INNER JOIN Products p ON od.ProductID = p.ProductID
+INNER JOIN Orders o ON od.OrderID = o.OrderID
+INNER JOIN Customers c ON c.CustomerID = o.CustomerID
+WHERE c.CompanyName = 'Alfreds Futterkiste'
+
+
+-- Denver bölgesine yapýlan satýþlar ve satýþý yapan personel ile 
+-- müþteri firma adý ve telefon bilgisi
+SELECT 
+	CONCAT(e.FirstName, ' ', e.LastName) Employee,
+	c.CompanyName,
+	c.ContactName,
+	c.Phone
+FROM Employees e
+INNER JOIN Orders o ON e.EmployeeID = o.EmployeeID
+INNER JOIN Customers c ON o.CustomerID = c.CustomerID
+INNER JOIN EmployeeTerritories et ON e.EmployeeID = et.EmployeeID
+INNER JOIN Territories t ON et.TerritoryID = t.TerritoryID
+WHERE t.TerritoryDescription = 'Denver'
+
+-- AGGREGATE FUNCTIONS
+-- Ürünler arasýndaki en yüksek etiket fiyatý
+SELECT MAX(UnitPrice) FROM Products
+SELECT TOP 1 UnitPrice FROM Products ORDER BY UnitPrice DESC
+
+SELECT MIN(UnitPrice) FROM Products
+-- 1997 Ocak ayýndaki sipariþlerin toplam cirosu
+SELECT SUM(od.UnitPrice * od.Quantity) FROM Orders o
+INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
+WHERE YEAR(o.OrderDate) = 1997 AND MONTH(o.OrderDate) = 1
+
+-- 1997 Ocak ayýndaki sipariþlerin sipariþ bazlý ara toplamlarý
+SELECT o.OrderID, SUM(od.UnitPrice * od.Quantity) FROM Orders o
+INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
+WHERE YEAR(o.OrderDate) = 1997 AND MONTH(o.OrderDate) = 1
+GROUP BY o.OrderID
+
+-- 1997 Ocak ayýndaki sipariþlerin gün bazlý ara toplamlarý
+SELECT o.OrderDate, SUM(od.UnitPrice * od.Quantity) FROM Orders o
+INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
+WHERE YEAR(o.OrderDate) = 1997 AND MONTH(o.OrderDate) = 1
+GROUP BY o.OrderDate
+
+-- 1997 Ocak ayýnda Ürün bazlý cirolar
+SELECT 
+	p.ProductName, 
+	SUM(od.UnitPrice * od.Quantity) Amount 
+FROM Orders o
+INNER JOIN [Order Details] od ON o.OrderID = od.OrderID
+INNER JOIN Products p ON od.ProductID = p.ProductID
+WHERE YEAR(o.OrderDate) = 1997 AND MONTH(o.OrderDate) = 1
+GROUP BY p.ProductName
+ORDER BY Amount DESC
+
+-- docker run --name dockersql -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=1q2w3e4R!" -p 11433:1433 -d mcr.microsoft.com/mssql/server:2022-latest
